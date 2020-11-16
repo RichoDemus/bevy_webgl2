f5e780798ee1ae47b3916eeb78444d22d79c708c
RichoDemus <git@richodemus.com>
2020-11-16 22:37:29 +0100

    Clean up build_example.sh and remove templating from index.html

--- build_example.sh
+++ build_example.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+set -eo pipefail
+
 export EXAMPLE=$1
-export TS=$(date -u --iso-8601=seconds)
 RELEASE=${2:-release}
 if [ $RELEASE == "release" ]; then
    RELEASE_OPTS="--release"
@@ -7,11 +9,10 @@ fi
 OUT_DIR=bevy-showcase
 mkdir -p ${OUT_DIR}/wasm
 cp -a assets/ ${OUT_DIR}
+cp -a index.html ${OUT_DIR}
 cargo build  $RELEASE_OPTS --example $EXAMPLE --target wasm32-unknown-unknown 
-wasm-bindgen --no-typescript --out-dir ${OUT_DIR}/wasm --out-name $EXAMPLE --target web ~/target/wasm32-unknown-unknown/$RELEASE/examples/${EXAMPLE}.wasm
-sed 's|\.js\$/, |\.js/, |' -i ${OUT_DIR}/wasm/${EXAMPLE}.js
-envsubst < template_index.html > $OUT_DIR/${EXAMPLE}.html
+wasm-bindgen --no-typescript --out-dir ${OUT_DIR}/wasm --out-name $EXAMPLE --target web target/wasm32-unknown-unknown/$RELEASE/examples/${EXAMPLE}.wasm
 echo
-echo http://127.0.0.1:4000/${EXAMPLE}.html
+echo http://127.0.0.1:4000/index.html?example=${EXAMPLE}
 #echo
 test -z "$SKIP_HTTP_SERVER" && basic-http-server -x $OUT_DIR
